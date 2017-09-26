f7a0c55353d489f8dc929e78a85be4e8b6e512b0
zgl1 <andrew.zhang@roxburylatin.org>
2017-09-25 23:54:43 -0400

    add indel and multiple vcf compression code

---
+++ convAutoEncoderBasesIndels.py
@@ -0,0 +1,311 @@
+"""create a convolutional autoencoder w/ Tensorflow for Indel's bases compression,
+indel bases are the output of convAutoEncoderBaseswithIndels.py, so have to run that first
+
+"""
+
+import tensorflow as tf
+import numpy as np
+import math
+import bits_array as ba
+from activations import lrelu
+import os
+#from libs.utils import corrupt
+import time
+
+GAPS_SEGMENT_LENGTH = 14400#14400 #must be pefect square 120x120
+GAPS_SEGMENT_LENGTH_SR = 120 #120 #120 #square root
+GAPS_COMPRESSION_RATIO = 64 #64 #(64*25) #64 #must be perfect square
+GAPS_COMPRESSION_RATIO_SR = 8 #8 #(8*5)#8
+LW_ROWS = GAPS_SEGMENT_LENGTH/GAPS_COMPRESSION_RATIO #decode layer's weight matrix rows, columns fix to SEGMENT LENGTH
+
+LEARNING_RATE = 0.01 #was 0.01
+# %%
+def autoencoder(input_shape=[None, GAPS_SEGMENT_LENGTH],
+                n_filters=[1, 5, 5, 1], #10, 10, 10
+                filter_sizes=[4, 4, 4, 4], #all 4s before
+                corruption=False):
+    """Build a deep denoising autoencoder w/ tied weights.
+    Parameters
+    ----------
+    input_shape : list, optional
+        Description
+    n_filters : list, optional
+        Description
+    filter_sizes : list, optional
+        Description
+    Returns
+    -------
+    x : Tensor
+        Input placeholder to the network
+    z : Tensor
+        Inner-most latent representation
+    y : Tensor
+        Output reconstruction of the input
+    cost : Tensor
+        Overall cost to use for training
+    Raises
+    ------
+    ValueError
+        Description
+    """
+    # %%
+    # input to the network
+    x = tf.placeholder(
+        tf.float32, input_shape, name='x')
+
+
+    # %%
+    # ensure 2-d is converted to square tensor.
+    if len(x.get_shape()) == 2:
+        x_dim = np.sqrt(x.get_shape().as_list()[1])
+        if x_dim != int(x_dim):
+            raise ValueError('Unsupported input dimensions')
+        x_dim = int(x_dim)
+        x_tensor = tf.reshape(
+            x, [-1, x_dim, x_dim, n_filters[0]])
+        print "img is 2d", x.get_shape(), tf.shape(x_tensor)
+    elif len(x.get_shape()) == 4:
+        x_tensor = x
+        print "img is 4d", x.get_shape
+    else:
+        raise ValueError('Unsupported input dimensions')
+    current_input = x_tensor
+
+    # %%
+    # Optionally apply denoising autoencoder
+    if corruption:
+        #current_input = corrupt(current_input)
+        pass
+
+
+    # %%
+    # Build the encoder
+    encoder = []
+    shapes = []
+    for layer_i, n_output in enumerate(n_filters[1:]):
+        n_input = current_input.get_shape().as_list()[3]
+        shapes.append(current_input.get_shape().as_list())
+        W = tf.Variable(
+            tf.random_uniform([
+                filter_sizes[layer_i],
+                filter_sizes[layer_i],
+                n_input, n_output],
+                -1.0 / math.sqrt(n_input),
+                1.0 / math.sqrt(n_input)))
+        #b = tf.Variable(tf.zeros([n_output]))
+        encoder.append(W)
+        if layer_i != 2: #change here if add or remove layers
+            output = tf.nn.sigmoid(
+                tf.nn.conv2d(
+                current_input, W, strides=[1, 1, 1, 1], padding='SAME'))
+        else:
+            output = tf.nn.sigmoid(
+                tf.nn.conv2d(
+                current_input, W, strides=[1,
+                        GAPS_COMPRESSION_RATIO_SR, GAPS_COMPRESSION_RATIO_SR, 1], padding='SAME'))
+            #tf.add(tf.nn.conv2d(
+            #    current_input, W, strides=[1, 1, 1, 1], padding='SAME'), b))
+        current_input = output
+
+    # %%
+    # store the latent representation
+    z = current_input
+
+    #use a linear layer for decoding
+    reshape = tf.reshape(z, [-1, LW_ROWS]) #compressed to 225
+    dim = reshape.get_shape()[1].value
+    LWeights = tf.Variable(
+                tf.random_uniform([
+                dim,
+                GAPS_SEGMENT_LENGTH],
+                -1.0 / math.sqrt(n_input),
+                1.0 / math.sqrt(n_input)))
+
+    #biases = _variable_on_cpu('biases', [384], tf.constant_initializer(0.1))
+    y = tf.nn.sigmoid(tf.matmul(reshape,LWeights))
+
+    # cost function measures pixel-wise difference
+    cost = tf.reduce_sum(tf.square(y - x))
+
+    # %%
+    return {'x': x, 'z': z, 'y': y, 'cost': cost, 'w': LWeights, 'e':encoder}
+
+def getBasesData(filename, segmentLength=GAPS_SEGMENT_LENGTH):
+    """
+    Reads data file containing bases (1 gap per line)
+    :param filename: file containing bases
+    :param segmentLength: length of each segment
+    :return: two numpy arrays, with the first being the data that fits cleanly into
+        the segment length (size of num_segments x segmentLength),
+        and the second being the remainder data that gets left over.
+    """
+    twoBasesToNumbers = {'AC': 1.0, 'AG': 2.0, 'AT': 3.0, 'CA': 4.0, 'CG': 5.0, 'CT': 6.0,
+                    'GA': 7.0, 'GC': 8.0, 'GT': 9.0, 'TA': 10.0, 'TC': 11.0, 'TG': 12.0,
+                    'AA': 13, 'CC':14, 'TT':15, 'GG':16}
+
+    print "Reading data..."
+    rawData = []
+
+    rawSNPs = np.load('ref_alt.npy')
+    i = 0
+    rawData2Bases = []
+    totalSNPs = 0
+    for base in rawSNPs:
+        if i == 0:
+            key0 = base
+            i += 1
+            totalSNPs += 1
+        elif i == 1:
+            key1 = base
+            i = 0
+            key = key0 + key1
+            if key in twoBasesToNumbers:
+                entry = twoBasesToNumbers[key]
+                if totalSNPs <= 20:
+                    print entry
+                rawData2Bases.append(entry)
+            else:
+                print "Can not find entry with bases: ", key
+            totalSNPs += 1
+    print "totalSNPs: ", totalSNPs
+
+    rawData = rawData2Bases[:(len(rawData2Bases) - len(rawData2Bases)%segmentLength)]
+
+    leftOverMatrixIndex = len(rawData) - (len(rawData) % (segmentLength))
+    data = np.ndarray((0, segmentLength), dtype=np.float32)
+    for segment in xrange(len(rawData[:leftOverMatrixIndex]) / segmentLength):
+        data = np.vstack((data, np.asarray(rawData[(segment * segmentLength):((segment + 1) * segmentLength)])))
+    print data.shape[0], data.shape[1]
+
+    return data
+
+def normalize(data):
+    """
+    Normalizes data to between 0 and 1
+    :param data: data to be normalized
+    :return: normalized data
+    """
+    #average = data.max()/2.0
+    #print "average is in normalize: ", average
+    #return (data-average)/average
+    print data.max(), data.min()
+    return (data - data.min()) / (data.max() - data.min())
+
+def denormalize(data, minValue, maxValue, average):
+    """
+    Inverse of normalize()
+    :param data: data to be denormalized
+    :return:
+    """
+    #print "average in denorm and the decoded data before denom: ", average, data
+    #return data*average+average
+    print maxValue, minValue
+    return (data * (maxValue - minValue)) + minValue
+
+def findErrorMatrix(input, reconstructedInput):
+    """
+    Creates the error matrix used in decompression
+    :param input: original matrix
+    :param reconstructedInput: matrix, having  been reconstructed after compression
+    :return: error matrix containing position and difference of each inconsistency between the two matrices
+    """
+    errorMatrix = [[]]
+    errorMatrixOriginal = input - reconstructedInput
+    for i in xrange(errorMatrixOriginal.shape[0]):
+        for j in xrange(errorMatrixOriginal.shape[1]):
+            if errorMatrixOriginal[i,j] != 0:
+                errorMatrix.append([i, j, errorMatrixOriginal[i, j]])
+    print errorMatrix[0]
+    del(errorMatrix[0])
+    return errorMatrix
+
+def test_bases():
+    """Test the convolutional autoencder using MNIST."""
+    # %%
+    import tensorflow as tf
+    import tensorflow.examples.tutorials.mnist.input_data as input_data
+    import matplotlib.pyplot as plt
+
+    # %%
+    # load gaps as before
+    bases = getBasesData('bases')
+    norm_img = normalize(bases)
+    ae = autoencoder()
+
+    # %%
+    learning_rate = LEARNING_RATE #was 0.007
+    optimizer = tf.train.AdamOptimizer(learning_rate).minimize(ae['cost'])
+    start_time = time.time()
+
+    # %%
+    # We create a session to use the graph
+    sess = tf.Session()
+    sess.run(tf.global_variables_initializer())
+
+    # %%
+    # Fit all training data
+    batch_size = bases.shape[0]
+    n_epochs = 8000 #60000 #55000  before this test
+    for epoch_i in range(n_epochs):
+        for batch_i in range(bases.shape[0] // batch_size):
+            batch_xs = norm_img
+            #print batch_xs
+            train = np.array([img for img in batch_xs])
+            sess.run(optimizer, feed_dict={ae['x']: train})
+        print(epoch_i, sess.run(ae['cost'], feed_dict={ae['x']: train}))
+        y_whendone = sess.run(ae['y'], feed_dict={ae['x']: train})
+        z_whendone = sess.run(ae['z'], feed_dict={ae['x']: train})
+        w_whendone = sess.run(ae['w'], feed_dict={ae['x']: train})
+        eWeights_whendone = sess.run(ae['e'], feed_dict={ae['x']: train})
+        cost_now = sess.run(ae['cost'], feed_dict={ae['x']: train})
+        if cost_now < 80:
+            print "cost reaches 80"
+            break
+    end_time = time.time()
+    print "Autoencoder comprepss time in seconds: {0:.3f} ".format(end_time - start_time)
+    print "encoder weights", eWeights_whendone
+    for w in eWeights_whendone:
+        print w.shape
+
+    #print  "z shape", z_whendone.shape, z_whendone
+    y_img = np.reshape(y_whendone, (-1, GAPS_SEGMENT_LENGTH ))
+
+    recovered_bases = denormalize(y_img, bases.min(), bases.max(), 0) #last param not used
+    x_dim = recovered_bases.shape[0]
+
+    y_dim = recovered_bases.shape[1]
+    print x_dim, y_dim
+    for i in range(x_dim):
+        for j in range(y_dim):
+            recovered_bases[i][j] = round(recovered_bases[i][j])
+    #print recovered_gaps
+    errorMatrix = findErrorMatrix(bases, recovered_bases)
+    accuracy = 100 - (len(errorMatrix) / (float(x_dim*y_dim))) * 100.0
+
+    #fix the errors
+    for i in xrange(len(errorMatrix)):
+        row, col, error = errorMatrix[i]
+        recovered_bases[row, col] += error
+
+    assert np.array_equal(bases[0], recovered_bases[0])
+
+    print accuracy, len(errorMatrix), (x_dim*y_dim)
+    cost_whendone = np.sum(np.square(y_img - norm_img))
+    print cost_whendone
+    np.save('encodedDataIndels.npy', np.asarray(z_whendone, dtype=np.float16))
+    np.save('decoderIndels.npy', np.asarray(w_whendone, dtype=np.float16))
+    np.save('errorMatrixIndels.npy', np.asarray(errorMatrix, dtype=np.int8))
+    #np.save('encoder.npy', np.asarray(eWeights_whendone, dtype=np.float16))
+    #print gaps
+
+    # %%
+    # Plot example reconstructions
+
+    #recon = sess.run(ae['y'], feed_dict={ae['x']: norm_img})
+
+    #print norm_img
+    #print "recon", recon.shape
+    #print recon
+
+if __name__ == '__main__':
+    test_bases()
---
+++ convAutoEncoderJWChr1with5Alts.py
@@ -0,0 +1,313 @@
+"""
+Compress chrom 1 of JW, JW with 1000 random alts, 2000 alts, ...and 5000 alts
+then calculate the  similarity coefficients. use JWIndel.tar.gz as input data
+"""
+
+import tensorflow as tf
+import numpy as np
+import math
+from activations import lrelu
+import os
+#from libs.utils import corrupt
+import time
+
+GAPS_SEGMENT_LENGTH = 14400#14400 #must be pefect square 120x120
+GAPS_SEGMENT_LENGTH_SR = 120 #120 #120 #square root
+GAPS_COMPRESSION_RATIO = 64 #64 #(64*25) #64 #must be perfect square
+GAPS_COMPRESSION_RATIO_SR = 8 #8 #(8*5)#8
+LW_ROWS = GAPS_SEGMENT_LENGTH/GAPS_COMPRESSION_RATIO #decode layer's weight matrix rows, columns fix to SEGMENT LENGTH
+
+LEARNING_RATE = 0.01
+# %%
+def autoencoder(input_shape=[None, GAPS_SEGMENT_LENGTH],
+                n_filters=[1, 5, 5, 1], #10, 10, 10
+                filter_sizes=[4, 4, 4, 4], #10, 10, 10
+                corruption=False):
+    """Build a deep denoising autoencoder w/ tied weights.
+    Parameters
+    ----------
+    input_shape : list, optional
+        Description
+    n_filters : list, optional
+        Description
+    filter_sizes : list, optional
+        Description
+    Returns
+    -------
+    x : Tensor
+        Input placeholder to the network
+    z : Tensor
+        Inner-most latent representation
+    y : Tensor
+        Output reconstruction of the input
+    cost : Tensor
+        Overall cost to use for training
+    Raises
+    ------
+    ValueError
+        Description
+    """
+    # %%
+    # input to the network
+    x = tf.placeholder(
+        tf.float32, input_shape, name='x')
+
+
+    # %%
+    # ensure 2-d is converted to square tensor.
+    if len(x.get_shape()) == 2:
+        x_dim = np.sqrt(x.get_shape().as_list()[1])
+        if x_dim != int(x_dim):
+            raise ValueError('Unsupported input dimensions')
+        x_dim = int(x_dim)
+        x_tensor = tf.reshape(
+            x, [-1, x_dim, x_dim, n_filters[0]])
+        print "img is 2d", x.get_shape(), tf.shape(x_tensor)
+    elif len(x.get_shape()) == 4:
+        x_tensor = x
+        print "img is 4d", x.get_shape
+    else:
+        raise ValueError('Unsupported input dimensions')
+    current_input = x_tensor
+
+    # %%
+    # Optionally apply denoising autoencoder
+    if corruption:
+        #current_input = corrupt(current_input)
+        pass
+
+
+    # %%
+    # Build the encoder
+    encoder = []
+    shapes = []
+    for layer_i, n_output in enumerate(n_filters[1:]):
+        n_input = current_input.get_shape().as_list()[3]
+        shapes.append(current_input.get_shape().as_list())
+        W = tf.Variable(
+            tf.random_uniform([
+                filter_sizes[layer_i],
+                filter_sizes[layer_i],
+                n_input, n_output],
+                -1.0 / math.sqrt(n_input),
+                1.0 / math.sqrt(n_input)))
+        #b = tf.Variable(tf.zeros([n_output]))
+        encoder.append(W)
+        if layer_i != 2: #change here if add or remove layers
+            output = tf.nn.sigmoid(
+                tf.nn.conv2d(
+                current_input, W, strides=[1, 1, 1, 1], padding='SAME'))
+        else:
+            output = tf.nn.sigmoid(
+                tf.nn.conv2d(
+                current_input, W, strides=[1,
+                        GAPS_COMPRESSION_RATIO_SR, GAPS_COMPRESSION_RATIO_SR, 1], padding='SAME'))
+            #tf.add(tf.nn.conv2d(
+            #    current_input, W, strides=[1, 1, 1, 1], padding='SAME'), b))
+        current_input = output
+
+    # %%
+    # store the latent representation
+    z = current_input
+
+    #use a linear layer for decoding
+    reshape = tf.reshape(z, [-1, LW_ROWS]) #compressed to 225
+    dim = reshape.get_shape()[1].value
+    LWeights = tf.Variable(
+                tf.random_uniform([
+                dim,
+                GAPS_SEGMENT_LENGTH],
+                -1.0 / math.sqrt(n_input),
+                1.0 / math.sqrt(n_input)))
+
+    #biases = _variable_on_cpu('biases', [384], tf.constant_initializer(0.1))
+    y = tf.nn.sigmoid(tf.matmul(reshape,LWeights))
+
+    # cost function measures pixel-wise difference
+    cost = tf.reduce_sum(tf.square(y - x))
+
+    # %%
+    return {'x': x, 'z': z, 'y': y, 'cost': cost, 'w': LWeights, 'e':encoder}
+
+def getBasesData(alts, segmentLength = GAPS_SEGMENT_LENGTH):
+    """
+    Reads data file containing bases (1 gap per line)
+    :param filename: file containing bases
+    :param segmentLength: length of each segment
+    :return: two numpy arrays, with the first being the data that fits cleanly into
+        the segment length (size of num_segments x segmentLength),
+        and the second being the remainder data that gets left over.
+    """
+    snpToNumbers = {'AC': 1.0, 'AG': 2.0, 'AT': 3.0, 'CA': 4.0, 'CG': 5.0, 'CT': 6.0,
+                    'GA': 7.0, 'GC': 8.0, 'GT': 9.0, 'TA': 10.0, 'TC': 11.0, 'TG': 12.0}
+
+    print "Reading data..."
+    rawData = []
+
+    totalBases = 0
+    for alt in alts:
+        filename = "/home/tester/data/JW/"+"bases2chr1"+alt
+
+        f = open(filename)
+        rawDataOneGenoAllSz = [x.strip() for x in f.readlines()]
+        rawDataOneGeno = [snpToNumbers[x] for x in rawDataOneGenoAllSz if x in snpToNumbers]
+
+        rawDataOneGenoRounded = rawDataOneGeno[:(len(rawDataOneGeno) - len(rawDataOneGeno)%segmentLength)]
+
+        print len(rawDataOneGeno), len(rawDataOneGenoRounded)
+        totalBases += len(rawDataOneGenoAllSz)
+
+        rawData.extend(rawDataOneGenoRounded)
+        #print rawData
+Alt
+    print totalBases
+
+    leftOverMatrixIndex = len(rawData) - (len(rawData) % (segmentLength))
+    data = np.ndarray((0, segmentLength), dtype=np.float32)
+    for segment in xrange(len(rawData[:leftOverMatrixIndex]) / segmentLength):
+        data = np.vstack((data, np.asarray(rawData[(segment * segmentLength):((segment + 1) * segmentLength)])))
+    print data.shape[0], data.shape[1]
+    return data
+
+def normalize(data):
+    """
+    Normalizes data to between 0 and 1
+    :param data: data to be normalized
+    :return: normalized data
+    """
+    #average = data.max()/2.0
+    #print "average is in normalize: ", average
+    #return (data-average)/average
+    print data.max(), data.min()
+    return (data - data.min()) / (data.max() - data.min())
+
+def denormalize(data, minValue, maxValue, average):
+    """
+    Inverse of normalize()
+    :param data: data to be denormalized
+    :return:
+    """
+    #print "average in denorm and the decoded data before denom: ", average, data
+    #return data*average+average
+    print maxValue, minValue
+    return (data * (maxValue - minValue)) + minValue
+
+def findErrorMatrix(input, reconstructedInput):
+    """
+    Creates the error matrix used in decompression
+    :param input: original matrix
+    :param reconstructedInput: matrix, having  been reconstructed after compression
+    :return: error matrix containing position and difference of each inconsistency between the two matrices
+    """
+    errorMatrix = [[]]
+    errorMatrixOriginal = input - reconstructedInput
+    for i in xrange(errorMatrixOriginal.shape[0]):
+        for j in xrange(errorMatrixOriginal.shape[1]):
+            if errorMatrixOriginal[i,j] != 0:
+                errorMatrix.append([i, j, errorMatrixOriginal[i, j]])
+    print errorMatrix[0]
+    del(errorMatrix[0])
+    return errorMatrix
+
+def test_bases(alt):
+    """Test the convolutional autoencder using MNIST."""
+    # %%
+    import tensorflow as tf
+    import tensorflow.examples.tutorials.mnist.input_data as input_data
+    import matplotlib.pyplot as plt
+
+    # %%
+    # load gaps as before
+    bases = getBasesData(alt)
+    norm_img = normalize(bases)
+    ae = autoencoder()
+
+    # %%
+    learning_rate = LEARNING_RATE #was 0.007
+    optimizer = tf.train.AdamOptimizer(learning_rate).minimize(ae['cost'])
+    start_time = time.time()
+
+    # %%
+    # We create a session to use the graph
+    sess = tf.Session()
+    sess.run(tf.global_variables_initializer())
+
+    # %%
+    # Fit all training data
+    batch_size = bases.shape[0]
+    n_epochs = 2000 #60000 #55000  before this test
+    for epoch_i in range(n_epochs):
+        for batch_i in range(bases.shape[0] // batch_size):
+            batch_xs = norm_img
+            #print batch_xs
+            train = np.array([img for img in batch_xs])
+            sess.run(optimizer, feed_dict={ae['x']: train})
+        if n_epochs%200 == 0:
+            print(epoch_i, sess.run(ae['cost'], feed_dict={ae['x']: train}))
+        y_whendone = sess.run(ae['y'], feed_dict={ae['x']: train})
+        z_whendone = sess.run(ae['z'], feed_dict={ae['x']: train})
+        w_whendone = sess.run(ae['w'], feed_dict={ae['x']: train})
+        eWeights_whendone = sess.run(ae['e'], feed_dict={ae['x']: train})
+        cost_now = sess.run(ae['cost'], feed_dict={ae['x']: train})
+        if cost_now < 10:
+            print "cost reaches 80"
+            break
+    end_time = time.time()
+    print "Autoencoder comprepss time in seconds: {0:.3f} ".format(end_time - start_time)
+    print "encoder weights", eWeights_whendone
+    for w in eWeights_whendone:
+        print w.shape
+
+    #print  "z shape", z_whendone.shape, z_whendone
+    y_img = np.reshape(y_whendone, (-1, GAPS_SEGMENT_LENGTH ))
+
+    recovered_bases = denormalize(y_img, bases.min(), bases.max(), 0) #last param not used
+    x_dim = recovered_bases.shape[0]
+    y_dim = recovered_bases.shape[1]
+    print x_dim, y_dim
+    for i in range(x_dim):
+        for j in range(y_dim):
+            recovered_bases[i][j] = round(recovered_bases[i][j])
+
+    #print recovered_gaps
+    errorMatrix = findErrorMatrix(bases, recovered_bases)
+    accuracy = 100 - (len(errorMatrix) / (float(x_dim*y_dim))) * 100.0
+
+    #fix the errors
+    for i in xrange(len(errorMatrix)):
+        row, col, error = errorMatrix[i]
+        recovered_bases[row, col] += error
+
+    assert np.array_equal(bases[0], recovered_bases[0])
+
+    print accuracy, len(errorMatrix), (x_dim*y_dim)
+    cost_whendone = np.sum(np.square(y_img - norm_img))
+    print cost_whendone
+    np.save('encodedData.npy', np.asarray(z_whendone, dtype=np.float16))
+    np.save('decoder.npy', np.asarray(w_whendone, dtype=np.float16))
+    np.save('errorMatrix.npy', np.asarray(errorMatrix, dtype=np.int8))
+
+    return z_whendone
+
+if __name__ == '__main__':
+    alts = ['alt0', 'alt1000', 'alt2000', 'alt3000', 'alt4000', 'alt5000']
+    encoder = test_bases(alts)
+    x_dim = encoder.shape[0]
+    y_dim = encoder.shape[1]
+    z_dim = encoder.shape[2]
+    print "encoder shapes: ", x_dim, y_dim, z_dim
+    all_indels_num = np.reshape(encoder, (6, x_dim*y_dim*z_dim/6))
+    alt0_encoder = all_indels_num[0, :]
+    alt1_encoder = all_indels_num[1, :]
+    alt2_encoder = all_indels_num[2, :]
+    alt3_encoder = all_indels_num[3, :]
+    alt4_encoder = all_indels_num[4, :]
+    alt5_encoder = all_indels_num[5, :]
+    sum  = np.sum(np.square(alt0_encoder))
+    sum1 = np.sum(np.square(alt1_encoder - alt0_encoder))
+    sum2 = np.sum(np.square(alt2_encoder - alt0_encoder))
+    sum3 = np.sum(np.square(alt3_encoder - alt0_encoder))
+    sum4 = np.sum(np.square(alt4_encoder - alt0_encoder))
+    sum5 = np.sum(np.square(alt5_encoder - alt0_encoder))
+    print sum1/sum, sum2/sum, sum3/sum, sum4/sum, sum5/sum
+
